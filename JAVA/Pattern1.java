ff74efc3042e7fc9e3c886557ea2f281a6a27a62
princettke <kushwahprince486@gmail.com>
2026-06-14 17:07:42 +0530

    commit 33 files to main

---
+++ JAVA/Pattern1.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Pattern1 {
+  public static void main(String [] args){
+// input by user 
+Scanner sc = new Scanner(System.in);
+int n = sc.nextInt();
+int m = sc.nextInt();
+
+for(int i=1;i<=n;i++){
+  for(int j =1;j<=m;j++){
+    System.out.print("*");
+  }
+  System.out.println();
+}
+
+  }
+}
+
+
+
+    // input by own
+  //   int n =4;
+  //   int m =5;
+  //   for(int i=1;i<=n;i++){
+      
+  //     for(int j=1;j<=m;j++){
+  //       System.out.print("*");
+  //     }
+  //     System.out.println();;
+  //       }
+  // }
+
+  
+
