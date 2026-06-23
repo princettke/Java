4b410a43c1f74deb07ab3b479657883017e48902
princettke <kushwahprince486@gmail.com>
2026-06-23 23:05:16 +0530

    commit 3 files to main

--- JAVA/Advancepattern_1.java
+++ JAVA/Advancepattern_1.java
@@ -9,6 +9,38 @@ public class Advancepattern_1 {
         
 
       }
+      // spaces 
+      int spaces = 2*(n-i);
+      for(int j=1;j<=spaces;j++){
+        System.out.print(" ");
+      }
+      //2 part
+      for(int j=1;j<=i;j++){
+        System.out.print("*");
+        
+      } 
+      System.out.println();
+    } 
+
+        // lower half
+    for(int i=n;i>=1;i--){
+      // 1st part 
+      for(int j=1;j<=i;j++){
+        System.out.print("*");
+        
+
+      }
+      // spaces 
+      int spaces = 2*(n-i);
+      for(int j=1;j<=spaces;j++){
+        System.out.print(" ");
+      }
+      //2 part
+      for(int j=1;j<=i;j++){
+        System.out.print("*");
+        
+      } 
+      System.out.println();
     } 
 
   }
