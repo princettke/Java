6b13a687afc3ee468261c784d160c3897cc13d6f
princettke <kushwahprince486@gmail.com>
2026-06-24 23:40:39 +0530

    commit 1 file to main

--- JAVA/Function.java
+++ JAVA/Function.java
@@ -1,6 +1,14 @@
+import java.util.*;
 public class Function {
-  public static void printMyName()
+  public static void printMyName(String name){
+    System.out.print(name);
+    return; // for exit the function 
+  }
   public static  void main (String [] args){
+    Scanner sc = new Scanner(System.in);
+    String name =sc.next();
+    printMyname(name); // call kiya function ko 
+
 
   }
   
