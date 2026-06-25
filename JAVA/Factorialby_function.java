74bd38eec5d5cc0269d3f1e19e9154ffa9ad64a0
princettke <kushwahprince486@gmail.com>
2026-06-25 23:59:03 +0530

    commit 2 files to main

--- JAVA/Factorialby_function.java
+++ JAVA/Factorialby_function.java
@@ -1,6 +1,11 @@
 import java.util.*;
 public class Factorialby_function {
   public static void printfactorial(int n){
+    // if number is neagtive 
+    if (n <0 ){
+      System.out.print("invaild number");
+      return;
+    }
     int factorial =1;
     for(int i=n;i>=1;i--){
     factorial = factorial*i;
