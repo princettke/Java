6b83a429cf8b8537b2fa2eb441f867eb7f9bf321
princettke <kushwahprince486@gmail.com>
2026-07-01 19:34:02 +0530

    commit 1 file to main

--- JAVA/Arrays/array1.java
+++ JAVA/Arrays/array1.java
@@ -1,6 +1,6 @@
 public class array1 {
   public static void main (String [] args){
-    int [] marks = new int[3];
+    int marks [] = new int[3];
     marks[0]=97;
     marks[1]=98;
     marks[2]=76;
